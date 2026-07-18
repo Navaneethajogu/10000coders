edc108c804c83da9d900cfd2730f41a1e17e7311
Navaneetha <you@example.com>
2026-07-18 21:09:21 +0530

    Added requirements.txt

--- demoproject/settings.py
+++ demoproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#kxccy6j86)-p_^#od!fdx_fz+m8tg1j0drqxj=x%$%8xu^q(!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ ".onrender.com",]
 
 
 # Application definition
